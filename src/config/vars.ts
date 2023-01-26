4df55376390929ba4920add31981453d20736467
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-26 12:19:15 -0300

    adjusting vars

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -10,10 +10,10 @@ export const config: {
   AWS_BUCKET_NAME: string
   AWS_BUCKET_REGION: string
 } = {
-  PORT: Number(process.env.PORT) ?? 8080,
-  MONGO_URI: String(process.env.MONGO_URI),
-  AWS_ACCESS_ID: String(process.env.AWS_ACCESS_KEY),
-  AWS_SECRET_ACCESS: String(process.env.AWS_SECRET_KEY),
-  AWS_BUCKET_NAME: String(process.env.AWS_BUCKET_NAME),
-  AWS_BUCKET_REGION: String(process.env.AWS_BUCKET_REGION),
+  PORT: Number(process.env.PORT) || 8080,
+  MONGO_URI: process.env.MONGO_URI || '',
+  AWS_ACCESS_ID: process.env.AWS_ACCESS_KEY || '',
+  AWS_SECRET_ACCESS: process.env.AWS_SECRET_KEY || '',
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || '',
+  AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION || '',
 }
